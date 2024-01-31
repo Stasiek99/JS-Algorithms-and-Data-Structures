c05f46ce0535787f90befc6f0d0472611cb17d8e
Stasiek99 <stasiekpilich@gmail.com>
2024-01-31 12:34:18 +0100

    performance of arrays and objects

---
+++ Performance of Arrays and Objects/object.js
@@ -0,0 +1,14 @@
+let instructor = {
+    firstname: "Kelly",
+    isInstructor: true,
+    favoriteNumbers: [1, 2, 3, 4]
+}
+
+// When to use objects:
+// when youd don't need order
+// When you need faster access / insertion and removal
+
+// Insertion - O(1) | Removal - O(1) | Searching - O(n) | Access - O(1)
+
+// Methods:
+// Object.keys - O(n) | Object.values - O(n) | Object.entries - O(n) | hasOwnProperty - O(1)
