5e63188ccacfecdfc3a50b9abba9c5748e1bfe1e
Stasiek99 <stasiekpilich@gmail.com>
2024-02-12 15:40:13 +0100

    frequency counters

--- Performance of Arrays and Objects/arrays.js
+++ Performance of Arrays and Objects/arrays.js
@@ -2,7 +2,7 @@ let names = ["Michael", "Melissa", "Andrea"];
 let values = [true, {}, [], 2, "awesome"];
 
 // When to use arrays:
-// when youd need order
+// when you need order
 // When you need faster access / insertion and removal (sort of...)
 
 // Insertion - It depends... | Removal - It depends... | Searching - O(n) | Access - O(1)
