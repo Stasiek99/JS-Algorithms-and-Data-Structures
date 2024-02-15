9d502058e38dced81b59772970717943ee143995
Stasiek99 <stasiekpilich@gmail.com>
2024-02-15 17:20:33 +0100

    assignments solutions

---
+++ Problem Solving Patterns/assignments/average_pair.js
@@ -0,0 +1,21 @@
+// My answer
+function averagePair(arr, float) {
+    if (arr.length < 2) {
+        return false;
+    }
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let average = (arr[left] + arr[right]) / 2;
+        if (average === float) {
+            return true;
+        } else if (average < float) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return false;
+}
+
