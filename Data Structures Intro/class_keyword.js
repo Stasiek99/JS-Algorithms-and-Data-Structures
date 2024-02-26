7a416c406bcdfbd0319e0fbe4e82fc0fb01a8607
Stasiek99 <103383789+Stasiek99@users.noreply.github.com>
2024-02-26 23:18:43 +0100

    data structures basics

---
+++ Data Structures Intro/class_keyword.js
@@ -0,0 +1,10 @@
+class Student {
+    constructor(firstName, lastName, year){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = year;
+    }
+}
+
+let firstStudent = new Student("Stasiek", "Kowalski",1);
+let secondStudent = new Student("Stasiek", "Nowak",2);
