1352eb0e9ebf4bc5a6c1e39054d0bd4b9fd80b40
Stasiek99 <stasiekpilich@gmail.com>
2024-02-27 22:13:02 +0100

    sll/shift and ssl/unshift

--- Singly Linked Lists/Singly_Linked_List_Structure.js
+++ Singly Linked Lists/Singly_Linked_List_Structure.js
@@ -42,4 +42,28 @@ class SinglyLinkedList {
         }
         return current;
     }
+
+    shift() {
+        if (!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return currentHead;
+    }
+
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = newNode;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
