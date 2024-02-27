a71a378b8163639183b7494ea649a18434417eec
Stasiek99 <stasiekpilich@gmail.com>
2024-02-28 00:10:40 +0100

    sll/insert, sll/remove and sll/reverse

--- Singly Linked Lists/Singly_Linked_List_Structure.js
+++ Singly Linked Lists/Singly_Linked_List_Structure.js
@@ -45,7 +45,7 @@ class SinglyLinkedList {
 
     shift() {
         if (!this.head) return undefined;
-        let currentHead = this.head;
+        const currentHead = this.head;
         this.head = currentHead.next;
         this.length--;
         if (this.length === 0) {
@@ -55,7 +55,7 @@ class SinglyLinkedList {
     }
 
     unshift(val) {
-        let newNode = new Node(val);
+        const newNode = new Node(val);
         if (!this.head) {
             this.head = newNode;
             this.tail = this.head;
@@ -85,5 +85,54 @@ class SinglyLinkedList {
             return true;
         }
         return false;
+    }
+
+    insert(index, val) {
+        if (index < 0 || index > this.length) return false;
+        if (index === this.length) return !!this.push(val);
+        if (index === 0) return !!this.unshift(val);
+
+        const newNode = new Node(val);
+        const prev = this.get(index - 1);
+        const temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return null
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+        const previousNode = this.get(index - 1);
+        const removed = previousNode.next;
+        previousNode.next = removed.next;
+        this.length--;
+        return removed;
+    }
+
+    reverse() {
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let next = null;
+        let prev = null;
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
+
+    print() {
+        const arr = [];
+        let current = this.head;
+        while (current) {
+            arr.push(current.val);
+            current = current.next;
+        }
     }
 }
